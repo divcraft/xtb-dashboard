e9e8500e34c354771a737aaa4a4f85ae0af83373
divcraft <kontakt@divcraft.pl>
2025-02-16 12:55:54 +0100

    init svelte and express apps

---
+++ __client/src/App.svelte
@@ -0,0 +1,15 @@
+<script>
+    let name = "Svelte";
+</script>
+
+<main>
+    <h1>Hello {name}!</h1>
+</main>
+
+<style>
+    main {
+        text-align: center;
+        padding: 2rem;
+        font-family: sans-serif;
+    }
+</style>
---
+++ __client/vite.config.ts
@@ -0,0 +1,17 @@
+import { defineConfig } from 'vite'
+import { svelte } from "@sveltejs/vite-plugin-svelte";
+
+export default defineConfig({
+    plugins: [svelte({
+        compilerOptions: {
+            compatibility: {
+              componentApi: 4,
+            },
+          },
+    })],
+    root: './__client',
+    server: {
+        // open: true,
+        strictPort: true,
+      },
+});
